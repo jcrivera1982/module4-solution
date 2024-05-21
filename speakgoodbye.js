e375daad29de68418afbbf0a35ac7fdab7d9ef16
jcrivera1982 <167439335+jcrivera1982@users.noreply.github.com>
2024-05-21 10:29:22 -0400

    Update speakgoodbye.js

--- speakgoodbye.js
+++ speakgoodbye.js
@@ -1,10 +1,14 @@
-(function (window){
-    var byeSpeaker = {};
-    var speakWord = "Good Bye";
-    
-    byeSpeaker.speak = function (names) {
-      console.log(speakWord + " " + names);
-    }
-     
-     window.byeSpeaker = byeSpeaker;
-    })(window);
+(function (window) {
+
+  var byeSpeaker = {};
+
+
+  var speakWord = "Good Bye";
+
+ byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
